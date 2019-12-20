7c79ffe4aea1cb56c6d53f66b13b9a9a07d366bf
unknown <you@example.com>
2019-12-20 10:19:37 +0200

    commit

---
+++ src/scripts/showDays.js
@@ -0,0 +1,14 @@
+const daysElment = document.querySelector('.week__days');
+
+const daysName =['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ','ВС'];
+
+
+//month 
+
+const monthElement = document.querySelector('.header__date');
+const monthsName = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+const setCurrentMonth =()=>{
+
+    const daysNumberElement = document.querySelector('.number')
+}
